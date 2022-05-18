464e2dbbcea250caed4e43a2f852600a4f471d19
GeethanjaliAnbalagan <84552111+GeethanjaliAnbalagan@users.noreply.github.com>
2022-05-18 12:29:26 +0530

    Add files via upload

---
+++ Oauth2/src/main/java/com/example/demo/Oauth2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@SpringBootApplication
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+}
